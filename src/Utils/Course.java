6ac3ff423c68ad76434d4803c39f964a367c3bda
dvatsav <deepak.srivatsav@gmail.com>
2017-10-13 23:33:12 +0530

    added required classes

--- src/Utils/Course.java
+++ src/Utils/Course.java
@@ -1,4 +1,13 @@
 package Utils;
 
+import java.util.Calendar;
+import java.util.List;
+import java.util.Map;
+
 public class Course {
+    private String postCondition;
+    private Calendar timing;
+    private Map<Calendar, String> roomAlloted;
+    private List<String> intendedAudience;
+    private int semesterOfferedIn;
 }
--- src/Utils/Room.java
+++ src/Utils/Room.java
@@ -1,4 +1,11 @@
 package Utils;
 
+import java.util.Calendar;
+import java.util.Map;
+
 public class Room {
+    private int totalCapacity;
+    private Map<Calendar, Boolean> roomAvailable;
+    private String roomNumber;
+
 }
