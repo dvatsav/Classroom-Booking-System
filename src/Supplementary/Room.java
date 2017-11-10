f89035de70a560ba7e32e2e704f08ca627a33019
dvatsav <deepak.srivatsav@gmail.com>
2017-11-11 01:02:36 +0530

    resized the window for all users, fixed table in student to display post conditions as well, modified the format of the csv file to use specific separators to try and parse data efficiently for storing timings. Also linked the book room button in student to the fxml page, havent yet done the same for admin and faculty.

--- src/Supplementary/Room.java
+++ src/Supplementary/Room.java
@@ -1,13 +1,40 @@
 package Supplementary;
 
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.HashMap;
 import java.util.Map;
 
 public class Room {
-    private int totalCapacity;
-    private Map<Calendar, Boolean> roomAvailable;
+    private Map<String, ArrayList<HashMap<String, Integer>>> roomAvailable = new HashMap<>();
     private String roomNumber;
 
+    public static void addTimings(String day, String data) {
+        if (data.equals("") || data.equals(" ")) {
+            return;
+        }
+        int occurences = 0;
+        ArrayList<HashMap<String, Integer>> temp = new ArrayList<>();
+        for (int i = 0 ; i < data.length() ; ++i) {
+            if (data.substring(i, i + 1).equals("@")) {
+                occurences += 1;
+            }
+        }
+        int startAt = 0;
+        int endAt = 0;
+        for (int i = 0 ; i < occurences ; ++i) {
+            startAt = data.indexOf("$", startAt) + 1;
+            String classNumber = "";
+            if (occurences > 1) {
+                endAt = data.indexOf("&", startAt);
+                classNumber = data.substring(startAt, endAt - 1);
+            }
+            else {
+                classNumber = data.substring(startAt);
+            }
+            System.out.println(classNumber);
+        }
+    }
     public String getRoomNumber() {return roomNumber;}
 
 }
