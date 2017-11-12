1685c36b4ef7602592af31d3926100e48bb62e46
dvatsav <deepak.srivatsav@gmail.com>
2017-11-12 23:19:39 +0530

    Room reqs from students can now be seen in table in admin login

--- src/UIControllers/AdminController.java
+++ src/UIControllers/AdminController.java
@@ -39,6 +39,62 @@ public class AdminController {
         BookingRequests b = new BookingRequests();
         b.setBookinRequests(b.deserialize());
         System.out.println(b.getBookingrequests());
+        tableanchor.getChildren().clear();
+        TableView tb = new TableView<>(generateDataInMap2(b));
+        tb.prefWidthProperty().bind(tableanchor.widthProperty());
+        tb.prefHeightProperty().bind(tableanchor.heightProperty());
+        TableColumn<Map, String> col1 = new TableColumn<>("Day");
+        TableColumn<Map, String> col2 = new TableColumn<>("Room Number");
+        TableColumn<Map, String> col3 = new TableColumn<>("Purpose");
+        TableColumn<Map, String> col4 = new TableColumn<>("Start Time");
+        TableColumn<Map, String> col5 = new TableColumn<>("End Time");
+        col1.setCellValueFactory(new MapValueFactory("Day"));
+        col2.setCellValueFactory(new MapValueFactory("Room Number"));
+        col3.setCellValueFactory(new MapValueFactory("Purpose"));
+        col4.setCellValueFactory(new MapValueFactory("Start Time"));
+        col5.setCellValueFactory(new MapValueFactory("End Time"));
+        tb.setEditable(true);
+        tb.getSelectionModel().setCellSelectionEnabled(true);
+        tb.getColumns().setAll(col1, col2, col3, col4, col5);
+        Callback<TableColumn<Map, String>,TableCell<Map, String>>
+                cellFactoryForMap = new Callback<TableColumn<Map, String>, TableCell<Map, String>>() {
+            @Override
+            public TableCell<Map, String> call(TableColumn<Map, String> p) {
+                return new TextFieldTableCell<>(new StringConverter() {
+                    @Override
+                    public String toString(Object t) {
+                        return t.toString();
+                    }
+
+                    @Override
+                    public String fromString(String string) {
+                        return string;
+                    }
+                });
+            }
+        };
+        col1.setCellFactory(cellFactoryForMap);
+        col2.setCellFactory(cellFactoryForMap);
+        col3.setCellFactory(cellFactoryForMap);
+        col4.setCellFactory(cellFactoryForMap);
+        col5.setCellFactory(cellFactoryForMap);
+        tableanchor.getChildren().add(tb);
+
+    }
+
+    public ObservableList<Map> generateDataInMap2(BookingRequests b) {
+        ObservableList<Map> allData = FXCollections.observableArrayList();
+        for (int i = 0; i < b.getBookingrequests().size() ; ++i) {
+            Map<String, String> dataRow = new HashMap<>();
+
+            dataRow.put("Day", (String)b.getBookingrequests().get(i).get("Day"));
+            dataRow.put("Room Number", (String)b.getBookingrequests().get(i).get("Room Number"));
+            dataRow.put("Purpose", (String)b.getBookingrequests().get(i).get("Purpose"));
+            dataRow.put("Start Time", (String)b.getBookingrequests().get(i).get("Start Time"));
+            dataRow.put("End Time", (String)b.getBookingrequests().get(i).get("End Time"));
+            allData.add(dataRow);
+        }
+        return allData;
     }
 
     @FXML
@@ -51,6 +107,25 @@ public class AdminController {
 
     @FXML
     public void handleCurrentBookings(ActionEvent event) {
+        setupTable();
+    }
+
+    private ObservableList<Map> generateDataInMap() {
+        ObservableList<Map> allData = FXCollections.observableArrayList();
+        for (int i = 0; i < Booking.bookings.size() ; ++i) {
+            Map<String, String> dataRow = new HashMap<>();
+
+            dataRow.put("Day", (String)Booking.bookings.get(i).get("Day"));
+            dataRow.put("Room Number", (String)Booking.bookings.get(i).get("Room Number"));
+            dataRow.put("Purpose", (String)Booking.bookings.get(i).get("Purpose"));
+            dataRow.put("Start Time", (String)Booking.bookings.get(i).get("Start Time"));
+            dataRow.put("End Time", (String)Booking.bookings.get(i).get("End Time"));
+            allData.add(dataRow);
+        }
+        return allData;
+    }
+
+    public void setupTable() {
         tableanchor.getChildren().clear();
         TableView tb = new TableView<>(generateDataInMap());
         tb.prefWidthProperty().bind(tableanchor.widthProperty());
@@ -91,21 +166,6 @@ public class AdminController {
         col4.setCellFactory(cellFactoryForMap);
         col5.setCellFactory(cellFactoryForMap);
         tableanchor.getChildren().add(tb);
-    }
-
-    private ObservableList<Map> generateDataInMap() {
-        ObservableList<Map> allData = FXCollections.observableArrayList();
-        for (int i = 0; i < Booking.bookings.size() ; ++i) {
-            Map<String, String> dataRow = new HashMap<>();
-
-            dataRow.put("Day", (String)Booking.bookings.get(i).get("Day"));
-            dataRow.put("Room Number", (String)Booking.bookings.get(i).get("Room Number"));
-            dataRow.put("Purpose", (String)Booking.bookings.get(i).get("Purpose"));
-            dataRow.put("Start Time", (String)Booking.bookings.get(i).get("Start Time"));
-            dataRow.put("End Time", (String)Booking.bookings.get(i).get("End Time"));
-            allData.add(dataRow);
-        }
-        return allData;
     }
 
     @FXML
