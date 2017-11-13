cfa908f4389633446b278adf9ea99aee5c9bdf36
Anubhav Chaudhary <achaudhary997@yahoo.in>
2017-11-14 02:26:16 +0530

    Fixed bugs in facultyController and faculty.xml

--- src/UIControllers/FacultyController.java
+++ src/UIControllers/FacultyController.java
@@ -48,46 +48,46 @@ public class FacultyController {
 
     @FXML
     public void handleCurrentBookings(ActionEvent event) {
-        tableanchor.getChildren().clear();
-        TableView tb = new TableView<>(generateDataInMap());
-        tb.prefWidthProperty().bind(tableanchor.widthProperty());
-        tb.prefHeightProperty().bind(tableanchor.heightProperty());
-        TableColumn<Map, String> col1 = new TableColumn<>("Day");
-        TableColumn<Map, String> col2 = new TableColumn<>("Room Number");
-        TableColumn<Map, String> col3 = new TableColumn<>("Purpose");
-        TableColumn<Map, String> col4 = new TableColumn<>("Start Time");
-        TableColumn<Map, String> col5 = new TableColumn<>("End Time");
-        col1.setCellValueFactory(new MapValueFactory("Day"));
-        col2.setCellValueFactory(new MapValueFactory("Room Number"));
-        col3.setCellValueFactory(new MapValueFactory("Purpose"));
-        col4.setCellValueFactory(new MapValueFactory("Start Time"));
-        col5.setCellValueFactory(new MapValueFactory("End Time"));
-        tb.setEditable(true);
-        tb.getSelectionModel().setCellSelectionEnabled(true);
-        tb.getColumns().setAll(col1, col2, col3, col4, col5);
-        Callback<TableColumn<Map, String>,TableCell<Map, String>>
-                cellFactoryForMap = new Callback<TableColumn<Map, String>, TableCell<Map, String>>() {
-            @Override
-            public TableCell<Map, String> call(TableColumn<Map, String> p) {
-                return new TextFieldTableCell<>(new StringConverter() {
-                    @Override
-                    public String toString(Object t) {
-                        return t.toString();
-                    }
+		tableanchor.getChildren().clear();
+		TableView tb = new TableView<>(generateDataInMap());
+		tb.prefWidthProperty().bind(tableanchor.widthProperty());
+		tb.prefHeightProperty().bind(tableanchor.heightProperty());
+		TableColumn<Map, String> col1 = new TableColumn<>("Day");
+		TableColumn<Map, String> col2 = new TableColumn<>("Room Number");
+		TableColumn<Map, String> col3 = new TableColumn<>("Purpose");
+		TableColumn<Map, String> col4 = new TableColumn<>("Start Time");
+		TableColumn<Map, String> col5 = new TableColumn<>("End Time");
+		col1.setCellValueFactory(new MapValueFactory("Day"));
+		col2.setCellValueFactory(new MapValueFactory("Room Number"));
+		col3.setCellValueFactory(new MapValueFactory("Purpose"));
+		col4.setCellValueFactory(new MapValueFactory("Start Time"));
+		col5.setCellValueFactory(new MapValueFactory("End Time"));
+		tb.setEditable(true);
+		tb.getSelectionModel().setCellSelectionEnabled(true);
+		tb.getColumns().setAll(col1, col2, col3, col4, col5);
+		Callback<TableColumn<Map, String>,TableCell<Map, String>>
+				cellFactoryForMap = new Callback<TableColumn<Map, String>, TableCell<Map, String>>() {
+			@Override
+			public TableCell<Map, String> call(TableColumn<Map, String> p) {
+				return new TextFieldTableCell<>(new StringConverter() {
+					@Override
+					public String toString(Object t) {
+						return t.toString();
+					}
 
-                    @Override
-                    public String fromString(String string) {
-                        return string;
-                    }
-                });
-            }
-        };
-        col1.setCellFactory(cellFactoryForMap);
-        col2.setCellFactory(cellFactoryForMap);
-        col3.setCellFactory(cellFactoryForMap);
-        col4.setCellFactory(cellFactoryForMap);
-        col5.setCellFactory(cellFactoryForMap);
-        tableanchor.getChildren().add(tb);
+					@Override
+					public String fromString(String string) {
+						return string;
+					}
+				});
+			}
+		};
+		col1.setCellFactory(cellFactoryForMap);
+		col2.setCellFactory(cellFactoryForMap);
+		col3.setCellFactory(cellFactoryForMap);
+		col4.setCellFactory(cellFactoryForMap);
+		col5.setCellFactory(cellFactoryForMap);
+		tableanchor.getChildren().add(tb);
     }
 
     private ObservableList<Map> generateDataInMap() {
