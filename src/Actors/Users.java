b8c1471f970b6045e0d7560eb0b2fcdf4451bae5
Anubhav Chaudhary <achaudhary997@yahoo.in>
2017-11-17 01:12:18 +0530

    Database working

--- src/Actors/Users.java
+++ src/Actors/Users.java
@@ -33,15 +33,25 @@ public class Users implements Serializable{
 		return firstName;
 	}
 
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
 
 	public String getLastName() {
 		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
 	}
 
 	public String getPhoneNumber() {
 		return phoneNumber;
 	}
 
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
 
 	public String getEmail() {
 		return email;
@@ -67,4 +77,11 @@ public class Users implements Serializable{
 		this.type = type;
 	}
 
+	public String getDob() {
+		return dob;
+	}
+
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
 }
